f0132c2754fc4f92417a7daa07418155b6152920
Pepijn Privé <pepurp.prive@gmail.com>
2025-11-15 00:20:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7323552408-3e774",
+  "appId": "1:841489483110:web:3a24872438b727a838f4a7",
+  "apiKey": "AIzaSyD8tFO7F1tbZQqHt2kuhVCx_7ur1koiqk4",
+  "authDomain": "studio-7323552408-3e774.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "841489483110"
+};
